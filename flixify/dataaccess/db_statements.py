83adddfda79f35323278f32eabf55fe3786ddecd
LLinders <l.linders@student.han.nl>
2018-05-10 12:17:05 +0200

    Refactoring: db_statements.py

--- flixify/dataaccess/db_statements.py
+++ flixify/dataaccess/db_statements.py
@@ -6,30 +6,43 @@ def insert_movie(movie_id, movie_title, movie_description, movie_year, movie_lan
         movie_year = 0
     insert_movie_statement = "INSERT INTO MOVIE VALUES ('{0}', '{1}', '{2}', {3}, '{4}')".format(movie_id, movie_title.replace("'", "''"), movie_description.replace("'", "''"), movie_year, movie_language)
     cursor.execute(insert_movie_statement)
-    for movie_genre in movie_genres:
-        insert_movie_genre_statement = "INSERT INTO GENRE_FOR_MOVIE VALUES ('{0}', '{1}')".format(movie_id, movie_genre)
-        print(insert_movie_genre_statement)
-        cursor.execute(insert_movie_genre_statement)
+    insert_genres_for_movie(movie_id, movie_genres)
 
-    for media in movie_media:
-        resolution = media
-        url = movie_media[media]
-        insert_movie_media_statement = "INSERT INTO MOVIE_MEDIA VALUES ('{0}', {1}, '{2}')".format(movie_id, resolution, url)
-        print(insert_movie_media_statement)
-        cursor.execute(insert_movie_media_statement)
+    insert_movie_media(movie_id, movie_media)
+    insert_movie_subtitles(movie_id, movie_subtitles)
+    cursor.commit()
+
+
+def insert_movie_subtitles(movie_id, movie_subtitles):
     for movie_subtitle_language in movie_subtitles:
         for movie_subtitle_properties in movie_subtitles[movie_subtitle_language]:
             id = movie_subtitle_properties['id']
             filename = movie_subtitle_properties['filename']
             src = movie_subtitle_properties['src']
             url = movie_subtitle_properties['url']
-            insert_movie_subtitle_statement = "INSERT INTO MOVIE_SUBTITLE VALUES ('{0}', '{1}', '{2}', '{3}')".format(id,
-                                                                                                     movie_id,
-                                                                                                     url,
-                                                                                                     movie_subtitle_language)
+            insert_movie_subtitle_statement = "INSERT INTO MOVIE_SUBTITLE VALUES ('{0}', '{1}', '{2}', '{3}')".format(
+                id,
+                movie_id,
+                url,
+                movie_subtitle_language)
             cursor.execute(insert_movie_subtitle_statement)
-    cursor.commit()
 
+
+def insert_movie_media(movie_id, movie_media):
+    for media in movie_media:
+        resolution = media
+        url = movie_media[media]
+        insert_movie_media_statement = "INSERT INTO MOVIE_MEDIA VALUES ('{0}', {1}, '{2}')".format(movie_id, resolution,
+                                                                                                   url)
+        print(insert_movie_media_statement)
+        cursor.execute(insert_movie_media_statement)
+
+
+def insert_genres_for_movie(movie_id, movie_genres):
+    for movie_genre in movie_genres:
+        insert_movie_genre_statement = "INSERT INTO GENRE_FOR_MOVIE VALUES ('{0}', '{1}')".format(movie_id, movie_genre)
+        print(insert_movie_genre_statement)
+        cursor.execute(insert_movie_genre_statement)
 
 # import json
 # a = json.dumps({"Name": "Robert",
