a4f98ea178fc9cebfb744463ddb39d7827f0b681
LLinders <l.linders@student.han.nl>
2018-05-10 11:49:14 +0200

    Database connection properties can now be specified in the settings.ini and are read in db_connection.py

--- flixify/dataaccess/db_connection.py
+++ flixify/dataaccess/db_connection.py
@@ -1,14 +1,18 @@
 import pyodbc
-import platform
-print(platform.node())
-# Specifying the ODBC driver, server name, database, etc. directly
-cnxn = 0
-if(platform.node() == "HP-Pav"):
-    print("Martijn")
-    cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER=HP-PAV;DATABASE=FLIXIFY_MOVIES;UID=sa;PWD=6836PT')
-else:
-    print("Luc")
-    cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER=localhost;DATABASE=FLIXIFY_MOVIES;UID=weetikhet;PWD=nice')
+import configparser
 
-cursor = cnxn.cursor()
 
+config = configparser.ConfigParser()
+config.read('../resources/settings.ini')  # Read settings from settings.ini file
+database_props = config['database_properties']
+conn = pyodbc.connect('DRIVER={{0}};SERVER={1};DATABASE={2};UID={3};PWD={4}'
+                      .format(database_props['driver'], database_props['server'], database_props['database'],
+                              database_props['login_uid'], database_props['password']))  # Build connection string
+# if(platform.node() == "HP-Pav"):
+#     print("Martijn")
+#     cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER=HP-PAV;DATABASE=FLIXIFY_MOVIES;UID=sa;PWD=6836PT')
+# else:
+#     print("Luc")
+#     cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER=localhost;DATABASE=FLIXIFY_MOVIES;UID=weetikhet;PWD=nice')
+
+cursor = conn.cursor()
